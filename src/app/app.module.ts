81e714a111402d6ce43a8d4a3c736b7785439be1
Edson <joaoedson.dantas@outlook.com>
2024-03-15 16:47:16 -0300

    wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,20 @@
+import { AngularFireModule } from '@angular/fire';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from 'src/environments/environment';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,11 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBiCycNPHMijQ1QsCVceFRN241jP0NeIhI',
+    authDomain: 'pushnotificationsangular-dccce.firebaseapp.com',
+    projectId: 'pushnotificationsangular-dccce',
+    storageBucket: 'pushnotificationsangular-dccce.appspot.com',
+    messagingSenderId: '793712534212',
+    appId: '1:793712534212:web:c7a4d01528519ba200df91',
+  },
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
